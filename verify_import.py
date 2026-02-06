d70557d390249e173469d79c11d1cb8954706cdb
Gokulkrishna-edstem <gokulkrishna.ab@edstem.com>
2026-02-06 15:46:18 +0530

    clearing the repo

--- verify_import.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append(os.getcwd())
-try:
-    from training.train import train_model
-    print("Import Successful")
-except Exception as e:
-    print(f"Import Failed: {e}")
